8912f8f0bec1157bf80ee1b6985fb4eea6cbe3d7
Giraudux <giraudux@gmail.com>
2016-05-04 01:48:50 +0200

    implement linked list (with sentinel)

--- src/linkedlist.c
+++ src/linkedlist.c
@@ -2,44 +2,105 @@
  *
  */
 
-void grdx_LinkedList_init(grdx_LinkedList_t * linkedList) {
-    list->head = grdx_LinkedList_nil(list);
-    list->tail = grdx_LinkedList_nil(list);
+#include "linkedlist.h"
+
+void grdx_LinkedList_init(grdx_LinkedList_t * list) {
     list->size = 0;
+    list->nil.data = NULL;
+    list->nil.prev = &(list->nil);
+    list->nil.next = &(list->nil);
 }
 
-grdx_LinkedListNode_t * grdx_LinkedList_find(grdx_LinkedList_t * list, grdx_LinkedListNode_t * node, int (*compare)(grdx_LinkedListNode_t *, grdx_LinkedListNode_t *)) {
-    return NULL;
+grdx_LinkedListNode_t * grdx_LinkedList_findFromHead(grdx_LinkedList_t * list, void * data, int (*compare)(const void *, const void *)) {
+    grdx_LinkedListNode_t * node;
+
+    node = list->nil.next;
+    while(node != &(list->nil) && !compare(data, node->data)) {
+        node = node->next;
+    }
+
+    return node;
 }
 
-void grdx_LinkedList_insert(grdx_LinkedList_t * list, grdx_LinkedListNode_t * node) {
-    ;
+grdx_LinkedListNode_t * grdx_LinkedList_findFromTail(grdx_LinkedList_t * list, void * data, int (*compare)(const void *, const void *)) {
+    grdx_LinkedListNode_t * node;
+
+    node = list->nil.prev;
+    while(node != &(list->nil) && !compare(data, node->data)) {
+        node = node->prev;
+    }
+
+    return node;
+}
+
+void grdx_LinkedList_insertFromHead(grdx_LinkedList_t * list, grdx_LinkedListNode_t * node, int (*compare)(const void *, const void *)) {
+    (void) list;
+    (void) node;
+    (void) compare;
+}
+
+void grdx_LinkedList_insertFromTail(grdx_LinkedList_t * list, grdx_LinkedListNode_t * node, int (*compare)(const void *, const void *)) {
+    (void) list;
+    (void) node;
+    (void) compare;
+}
+
+void grdx_LinkedList_insertHead(grdx_LinkedList_t * list, grdx_LinkedListNode_t * node) {
+    node->next = list->nil.next;
+    list->nil.next->prev = node;
+    list->nil.next = node;
+    node->prev = &(list->nil);
+    list->size++;
+}
+
+void grdx_LinkedList_insertTail(grdx_LinkedList_t * list, grdx_LinkedListNode_t * node) {
+    node->prev = list->nil.prev;
+    list->nil.prev->next = node;
+    list->nil.prev = node;
+    node->next = &(list->nil);
+    list->size++;
 }
 
 void grdx_LinkedList_remove(grdx_LinkedList_t * list, grdx_LinkedListNode_t * node) {
-    ;
+    node->prev->next = node->next;
+    node->next->prev = node->prev;
+    list->size--;
 }
 
 grdx_LinkedListNode_t * grdx_LinkedList_head(grdx_LinkedList_t * list) {
-    return list->head;
+    return list->nil.next;
 }
 
 grdx_LinkedListNode_t * grdx_LinkedList_tail(grdx_LinkedList_t * list) {
-    return list->tail;
+    return list->nil.prev;
 }
 
 size_t grdx_LinkedList_size(grdx_LinkedList_t * list) {
     return list->size;
 }
 
-/* const */ grdx_LinkedListNode_t * grdx_LinkedList_nil(grdx_LinkedList_t * list) {
-    /* ignored parameter */
-    (void) list;
-
-    return NULL;
+const  grdx_LinkedListNode_t * grdx_LinkedList_nil(grdx_LinkedList_t * list) {
+    return &(list->nil);
 }
 
-void grdx_LinkedListNode_init(grdx_LinkedListNode_t * node);
-grdx_LinkedListNode_t * grdx_LinkedListNode_next(grdx_LinkedListNode_t * node);
-grdx_LinkedListNode_t * grdx_LinkedListNode_prev(grdx_LinkedListNode_t * node);
-void * grdx_LinkedListNode_data(grdx_LinkedListNode_t * node);
+void grdx_LinkedListNode_init(grdx_LinkedListNode_t * node) {
+    node->data = NULL;
+    node->prev = NULL;
+    node->next = NULL;
+}
+
+grdx_LinkedListNode_t * grdx_LinkedListNode_next(grdx_LinkedListNode_t * node) {
+    return node->next;
+}
+
+grdx_LinkedListNode_t * grdx_LinkedListNode_prev(grdx_LinkedListNode_t * node) {
+    return node->prev;
+}
+
+void * grdx_LinkedListNode_getData(grdx_LinkedListNode_t * node) {
+    return node->data;
+}
+
+void grdx_LinkedListNode_setData(grdx_LinkedListNode_t * node, void * data) {
+    node->data = data;
+}
--- src/linkedlist.h
+++ src/linkedlist.h
@@ -9,11 +9,9 @@
 
 struct grdx_LinkedListNode_s;
 struct grdx_LinkedList_s;
-struct grdx_LinkedListSentinel_s;
 
-typedef grdx_LinkedListNode_s grdx_LinkedListNode_t;
-typedef grdx_LinkedList_s grdx_LinkedList_t;
-typedef grdx_LinkedListSentinel_s grdx_LinkedListSentinel_t;
+typedef struct grdx_LinkedListNode_s grdx_LinkedListNode_t;
+typedef struct grdx_LinkedList_s grdx_LinkedList_t;
 
 struct grdx_LinkedListNode_s {
     void * data;
@@ -23,20 +21,14 @@ struct grdx_LinkedListNode_s {
 
 struct grdx_LinkedList_s {
     size_t size;
-    grdx_LinkedListNode_t * head;
-    grdx_LinkedListNode_t * tail;
-};
-
-struct grdx_LinkedListSentinel_s {
-    size_t size;
     grdx_LinkedListNode_t nil;
 };
 
-/* inline */
-
 void grdx_LinkedList_init(grdx_LinkedList_t * list);
-grdx_LinkedListNode_t * grdx_LinkedList_find(grdx_LinkedList_t * list, grdx_LinkedListNode_t * node, int (*compare)(grdx_LinkedListNode_t *, grdx_LinkedListNode_t *));
-void grdx_LinkedList_insert(grdx_LinkedList_t * list, grdx_LinkedListNode_t * node, int (*compare)(grdx_LinkedListNode_t *, grdx_LinkedListNode_t *));
+grdx_LinkedListNode_t * grdx_LinkedList_findFromHead(grdx_LinkedList_t * list, void * data, int (*compare)(const void *, const void *));
+grdx_LinkedListNode_t * grdx_LinkedList_findFromTail(grdx_LinkedList_t * list, void * data, int (*compare)(const void *, const void *));
+void grdx_LinkedList_insertFromHead(grdx_LinkedList_t * list, grdx_LinkedListNode_t * node, int (*compare)(const void *, const void *));
+void grdx_LinkedList_insertFromTail(grdx_LinkedList_t * list, grdx_LinkedListNode_t * node, int (*compare)(const void *, const void *));
 void grdx_LinkedList_insertHead(grdx_LinkedList_t * list, grdx_LinkedListNode_t * node);
 void grdx_LinkedList_insertTail(grdx_LinkedList_t * list, grdx_LinkedListNode_t * node);
 void grdx_LinkedList_remove(grdx_LinkedList_t * list, grdx_LinkedListNode_t * node);
@@ -49,6 +41,6 @@ void grdx_LinkedListNode_init(grdx_LinkedListNode_t * node);
 grdx_LinkedListNode_t * grdx_LinkedListNode_next(grdx_LinkedListNode_t * node);
 grdx_LinkedListNode_t * grdx_LinkedListNode_prev(grdx_LinkedListNode_t * node);
 void * grdx_LinkedListNode_getData(grdx_LinkedListNode_t * node);
-void grdx_LinkedListNode_setData(grdx_LinkedListNode_t * node);
+void grdx_LinkedListNode_setData(grdx_LinkedListNode_t * node, void * data);
 
 #endif /* GRDX_LINKEDLIST_H */
